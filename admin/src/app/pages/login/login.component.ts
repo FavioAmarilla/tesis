43d5ab26f20764a19c802ec472546687bf3a6134
favio.amarilla <favio.amarilla@konecta.com.py>
2020-01-16 14:14:31 -0300

    se agrego efecto de carga al boton de loading

--- admin/src/app/pages/login/login.component.ts
+++ admin/src/app/pages/login/login.component.ts
@@ -5,25 +5,30 @@ import { User } from '../../models/user';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 
 export class LoginComponent implements OnInit {
 
   public user: User
   public errors = [];
+  public cargando: boolean = false;
 
   constructor(
     private userService: UserService,
     private router: Router
   ) {
     this.user = new User(null, null, null, null, null);
+    this.cargando = false;
   }
 
   ngOnInit() {
   }
 
   async login() {
+    this.cargando = true;
+
     this.errors = [];
     const response: any = await this.userService.login(this.user);
     if (response.success) {
@@ -31,6 +36,8 @@ export class LoginComponent implements OnInit {
     } else {
       this.errors.push('Credenciales no validas');
     };
+
+    this.cargando = false;
   }
 
 }
