797385c95829dfb1028ed95c2ddf1e2e32ef40c7
Favio <favio.amarilla@konecta.com.py>
2020-03-18 17:12:42 -0300

    correccion de relaciones

--- api/app/Sucursal.php
+++ api/app/Sucursal.php
@@ -20,16 +20,16 @@ class Sucursal extends Model
 
     //obtener empresa de la sucursal
     public function empresa(){
-        return $this->belongsTo('App\LineaProducto', 'id_empresa');
+        return $this->belongsTo('App\Empresa', 'id_empresa');
     }
 
     //obtener pais de la sucursal
     public function pais(){
-        return $this->belongsTo('App\LineaProducto', 'id_pais');
+        return $this->belongsTo('App\Pais', 'id_pais');
     }
 
     //obtener ciudad de la sucursal
     public function ciudad(){
-        return $this->belongsTo('App\LineaProducto', 'id_ciudad');
+        return $this->belongsTo('App\Ciudad', 'id_ciudad');
     }
 }
