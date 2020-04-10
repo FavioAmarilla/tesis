015f6a684a260754ed95157f27e52625ebdf7274
Favio <favio.amarilla@konecta.com.py>
2020-04-10 10:48:33 -0400

    actualizacion de rura de api

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // api: 'https://www.ecommercesy.com/api',
-    api: 'http://127.0.0.1:8000/api',
+    api: 'http://api.ecommercesy.com/api',
+    // api: 'http://127.0.0.1:8000/api',
     mapbox: {
         apiKey: 'pk.eyJ1IjoibWF0aWFzYmFlejI1IiwiYSI6ImNrODd0aW1tODAwMWEzbGtmb2t0amNnMzYifQ.TGRXCPLfAzvmp0-cEpeiDA',
         defaultCoords: {
