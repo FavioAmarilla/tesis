75f9f0ff93a3cf9d38aa38e365710f18aea7d82d
Favio <favio.amarilla@konecta.com.py>
2020-04-27 09:31:03 -0400

    se agrego menu lateral de linea de productos

--- app/src/app/paginas/inicio/inicio.page.ts
+++ app/src/app/paginas/inicio/inicio.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
 import { ProductoService } from '../../servicios/producto.service';
-import { Producto, Banner, LineaProducto } from '../../interfaces/interfaces';
+import { Producto, Banner, LineaProducto, Sucursal } from '../../interfaces/interfaces';
 import { Router } from '@angular/router';
 import { CarruselService } from 'src/app/servicios/carrusel.service';
 import { environment } from '../../../environments/environment';
@@ -9,7 +9,7 @@ import { CarritoService } from 'src/app/servicios/carrito.service';
 import { IonSlides } from '@ionic/angular';
 import { GeneralService } from 'src/app/servicios/general.service';
 import { AlertaService } from 'src/app/servicios/alerta.service';
-import { FiltrosComponent } from 'src/app/componentes/filtros/filtros.component';
+import { SucursalService } from 'src/app/servicios/sucursal.service';
 
 @Component({
   selector: 'app-inicio',
@@ -21,8 +21,11 @@ export class PaginaInicio implements OnInit {
   public API: string;
   public cargando = true;
 
+  public listaSucursales: Sucursal;
   public listaProductos: Producto;
   public listaSlides: Banner;
+  public listaLineas: LineaProducto;
+  public idLineaProducto: any = 0;
   public lineasProducto: LineaProducto;
 
   public slideImgUrl: string;
@@ -41,6 +44,8 @@ export class PaginaInicio implements OnInit {
     private servicioCarrito: CarritoService,
     private servicioProducto: ProductoService,
     private servicioCarrusel: CarruselService,
+    private servicioSucursal: SucursalService,
+    private servicioLineaProd: LineasProductoService,
     private servicioGeneral: GeneralService,
     private servicioAlerta: AlertaService,
     private router: Router
@@ -49,12 +54,75 @@ export class PaginaInicio implements OnInit {
     this.slideImgUrl = this.API + 'producto/getImage/';
   }
 
-  ngOnInit() {
-    this.obtenerCarrusel();
+  async ngOnInit() {
+    await this.obtenerCarrusel();
+    await this.obtenerLineaProducto();
+    await this.obtenerSucursales();
   }
 
   redireccionar(url) {
     this.router.navigate([url]);
+  }
+
+  async obtenerCarrusel() {
+    const response: any = await this.servicioCarrusel.obtenerCarrusel();
+
+    if (response.success) {
+      this.listaSlides = response.data;
+    } else {
+      this.cargando = false;
+      this.servicioAlerta.dialogoError(response.message, '');
+    }
+  }
+
+  async obtenerLineaProducto() {
+    const response: any = await this.servicioLineaProd.obtenerLinea();
+
+    if (response.success) {
+      this.listaLineas = response.data;
+    } else {
+      this.servicioAlerta.dialogoError(response.message, '');
+    }
+  }
+
+  async seleccionarLineaProducto(value) {
+    this.cargando = true;
+
+    let parametros = {
+      id_linea: value
+    }
+    this.idLineaProducto = await value;
+    await this.obtenerProductos(parametros);
+  }
+
+
+  async obtenerSucursales() {
+    const parametros = {
+      ecommerce: 'S'
+    };
+    const response: any = await this.servicioSucursal.obtenerSucursal(null, parametros);
+
+    if (response.success) {
+      this.listaSucursales = response.data;
+      const central = response.data.find(sucursal => sucursal.central == 'S');
+      if (central) {
+        this.seleccionarSucursal(central.identificador)
+        await this.servicioCarrito.setStorage('sucursal', central.identificador);
+      }
+    } else {
+      this.cargando = false;
+      this.servicioAlerta.dialogoError(response.message, '');
+    }
+  }
+
+  async seleccionarSucursal(value) {
+    this.cargando = true;
+
+    let parametros = {
+      id_sucursal: value
+    }
+    await this.servicioCarrito.setStorage('sucursal', value);
+    await this.obtenerProductos(parametros);
   }
 
   async obtenerProductos(parametros?) {
@@ -71,17 +139,6 @@ export class PaginaInicio implements OnInit {
       this.servicioAlerta.dialogoError(response.message, '');
     }
     this.cargando = false;
-  }
-
-  async obtenerCarrusel() {
-    const response: any = await this.servicioCarrusel.obtenerCarrusel();
-
-    if (response.success) {
-      this.listaSlides = response.data;
-    } else {
-      this.cargando = false;
-      this.servicioAlerta.dialogoError(response.message, '');
-    }
   }
 
   async agregarAlCarrito(producto: any) {
