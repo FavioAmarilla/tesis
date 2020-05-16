7e2a834f37c35a6dbfb7db0b5181e9726e593366
Matias Báez <matiasbaez2512@gmail.com>
2020-05-16 10:34:12 -0400

    Actualización de coordenadas por defecto

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
   mapbox: {
     apiKey: 'pk.eyJ1IjoibWF0aWFzYmFlejI1IiwiYSI6ImNrODd0aW1tODAwMWEzbGtmb2t0amNnMzYifQ.TGRXCPLfAzvmp0-cEpeiDA',
     defaultCoords: {
-      lat: '-25.4036867',
-      lng: '-57.2865209'
+      lat: '-25.403561',
+      lng: '-57.284329'
     }
   }
 };
