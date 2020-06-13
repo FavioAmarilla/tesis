b54483452872aef215f395ac589041b9f606c8a5
Favio <favio.amarilla@konecta.com.py>
2020-06-13 15:00:23 -0400

    correccion de tabs

--- app/src/app/paginas/mi-cuenta/mi-cuenta.module.ts
+++ app/src/app/paginas/mi-cuenta/mi-cuenta.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { MiCuentaPage } from './mi-cuenta.page';
 import { ModuloComponentes } from '../../componentes/componentes.module';
+import {MatTabsModule} from '@angular/material/tabs';
 
 const routes: Routes = [
   {
@@ -21,7 +22,8 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-    ModuloComponentes
+    ModuloComponentes,
+    MatTabsModule
   ],
   declarations: [MiCuentaPage]
 })
