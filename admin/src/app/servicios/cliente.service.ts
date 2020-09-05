3d592f00a0ec11d94f599ae231cfc7f730473a1a
Favio <favio.amarilla@konecta.com.py>
2020-09-05 11:20:11 -0400

    validacion de numero de documento

--- admin/src/app/servicios/cliente.service.ts
+++ admin/src/app/servicios/cliente.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'environments/environment';
+
+const API = environment.api;
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteService  extends BaseService {
+export class ClienteService extends BaseService {
 
   recurso = 'cliente';
+
+
+  async documento(numero) {
+    const url = `cliente/documento/${numero}`;
+
+    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return new Promise(resolve => {
+      this.http.get(`${API}${url}`, { headers }).subscribe(
+        (response: any) => {
+          resolve(response);
+        },
+        error => {
+          resolve(error.error);
+        }
+      );
+    });
+  }
 
 }
