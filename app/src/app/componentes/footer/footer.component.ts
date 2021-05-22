d3f95a59241938cbb6d25064c65126f43032e2e7
Matías Báez <matiasbaez2512@gmail.com>
2021-05-22 10:44:40 -0400

    Opcion para descargar apk

--- app/src/app/componentes/footer/footer.component.ts
+++ app/src/app/componentes/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
@@ -11,12 +12,17 @@ export class FooterComponent implements OnInit {
 
   @Input() bgLight = true;
 
+  isMobile = false;
+
   constructor(
+    private platform: Platform,
+    private iab: InAppBrowser,
     private router: Router,
-    private iab: InAppBrowser
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isMobile = this.platform.is('android') || this.platform.is('ios');
+  }
 
   openBrowser(url: string) {
     const browser = this.iab.create(url, '_system');
